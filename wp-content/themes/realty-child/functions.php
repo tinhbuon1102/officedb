4d07d7a311571d23a1b9265e67957416ed08ba4a
thangtqvn <quocthang.2001@gmail.com>
2017-04-20 14:14:18 +0700

    Upload files

--- wp-content/themes/realty-child/functions.php
+++ wp-content/themes/realty-child/functions.php
@@ -1,4 +1,7 @@
 <?php
+define('SITE_DATE_FORMAT', 'Y年m月d日');
+define('SITE_TIME_FORMAT', 'G:i');
+
 /**
  * Loads the child theme textdomain.
  */
@@ -24,6 +27,15 @@ function custom_scripts ()
 }
 add_action('wp_enqueue_scripts', 'custom_scripts');
 
+//ショートコードを使ったphpファイルの呼び出し方法
+function my_php_Include($params = array()) {
+	extract(shortcode_atts(array('file' => 'default'), $params));
+	ob_start();
+	include(STYLESHEETPATH . "/template-parts/$file.php");
+	return ob_get_clean();
+}
+add_shortcode('myphp', 'my_php_Include');
+
 function getAvailablePrefectures() {
 	return array('東京都');
 }
@@ -44,63 +56,56 @@ function jpn_mccompare($a, $b) {
 
 function getDateFormat($hasTime = true)
 {
-	$format = 'Y年m月d日';
-	$format .= $hasTime ? ' G:i' : '';
+	$format = SITE_DATE_FORMAT;
+	$format .= $hasTime ? ' ' . SITE_TIME_FORMAT : '';
 	return $format;
 }
 
 function getTimeFormat($time)
 {
-	$format = 'G:i';
+	$format = SITE_TIME_FORMAT;
 	return date($format, strtotime($time));
 }
 
-function getDateFromStrTime($strTime)
+function getDateFromStampTime($stmTime)
 {
-	return date('Y-m-d H:i:s', $strTime);
+	return date('Y-m-d H:i:s', $stmTime);
 }
 
-function renderJapaneseDate($date, $hasTime = true)
+function renderJapaneseDate($date, $hasTime = false)
 {
 	return date(getDateFormat($hasTime), strtotime($date));
 }
 
-add_action('init', 'realty_theme_init', 10, 3);
-function realty_theme_init() {
-	
-	$recent_posts = wp_get_recent_posts(array('post_type'=>'news'));
-	pr(renderJapaneseDate($recent_posts[0]['post_date']));die;
-	foreach( $recent_posts as $recent ){
-		echo '<li class="col-sm-4"><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" ><div class="post-date">' .   $recent["post_time"] .'</div>'.$recent["post_title"].'</a> </li> ';
-	}
-	
+// add_action('init', 'realty_theme_init', 10, 3);
+function realty_theme_init()
+{
 	// Import new location
-// 	importLocationFromPrefecture ();
+	// 	importLocationFromPrefecture ();
 }
 function importLocationFromPrefecture () {
-// 	return ;
 	// Delete old location;
 	$terms = get_terms( array(
 		'taxonomy' => 'property-location',
 		'hide_empty' => false,
 	) );
-	
+
 	pr($terms);die;
 	if (!empty($terms))
 	{
-		// Don't reimport 
+		// Don't reimport
 		if (count($terms) >= 50) return;
-		
+
 		foreach ($terms as $term)
 		{
 			wp_delete_term($term->term_id, $term->taxonomy);
 		}
 	}
-	
+
 	$dir = ABSPATH . 'dataAddress/zipcode';
 	$cities = array();
 	$prefectures = getAvailablePrefectures();
-	
+
 	for($i=0; $i<10; $i++)
 	{
 		$file = $dir . DIRECTORY_SEPARATOR . $i. '.csv';
@@ -114,22 +119,13 @@ function importLocationFromPrefecture () {
 			}
 		}
 	}
-	
+
 	setlocale(LC_COLLATE, "jpn");
 	usort ($cities, "jpn_mccompare");
 	foreach ($cities as $city)
 	{
 		wp_insert_term( $city, 'property-location');
 	}
-	
+
 	return $cities;
 }
-
-//ショートコードを使ったphpファイルの呼び出し方法
-function my_php_Include($params = array()) {
- extract(shortcode_atts(array('file' => 'default'), $params));
- ob_start();
- include(STYLESHEETPATH . "/template-parts/$file.php");
- return ob_get_clean();
-}
-add_shortcode('myphp', 'my_php_Include');
