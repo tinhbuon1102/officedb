1e1089a9f5717666f448ff5671d5148647d07f45
tinhbuon1102 <quocthang.2001@gmail.com>
2017-04-21 06:22:03 +0900

    Server Upload files

--- wp-content/themes/realty-child/template-parts/home-newslist.php
+++ wp-content/themes/realty-child/template-parts/home-newslist.php
@@ -2,7 +2,7 @@
 <?php
     $recent_posts = wp_get_recent_posts(array('post_type'=>'news'));
     foreach( $recent_posts as $recent ){
-        echo '<li class="col-sm-4"><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" ><div class="post-date">' .   $recent["post_time"] .'</div>'.$recent["post_title"].'</a> </li> ';
+        echo '<li class="col-sm-4"><div class="inner"><div class="post-date">' .   renderJapaneseDate($recent['post_date']) .'</div><div class="title">'.$recent["post_title"].'</div><div class="meta">'.get_the_category($recent["cat_name"]).'</div><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" class="check_now" >Check now</a></div></li> ';
     }
 ?>
 </ul>
