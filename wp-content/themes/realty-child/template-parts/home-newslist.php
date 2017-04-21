84b88a53ed034670696d07e334d08cdae83fcd1b
tinhbuon1102 <quocthang.2001@gmail.com>
2017-04-22 01:14:07 +0900

    Server Upload files

--- wp-content/themes/realty-child/template-parts/home-newslist.php
+++ wp-content/themes/realty-child/template-parts/home-newslist.php
@@ -1,8 +1,31 @@
 <ul class="latest-news-list row">
 <?php
-    $recent_posts = wp_get_recent_posts(array('post_type'=>'news'));
-    foreach( $recent_posts as $recent ){
-        echo '<li class="col-sm-4"><div class="inner"><div class="post-date">' .   renderJapaneseDate($recent['post_date']) .'</div><div class="title">'.$recent["post_title"].'</div><div class="meta">'.get_the_category($recent["cat_name"]).'</div><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" class="check_now" >Check now</a></div></li> ';
-    }
+$recent_posts = wp_get_recent_posts(array(
+	'post_type' => 'news'
+));
+
+foreach ( $recent_posts as $recent )
+{
+	$categories = get_the_category($recent['ID']); // post ID
+	?>
+<li class="col-sm-4">
+	<div class="inner">
+	<div class="con-inner">
+		<div class="post-date"><?php echo renderJapaneseDate($recent['post_date'])?></div>
+		<div class="title"><?php echo $recent["post_title"]?></div>
+		<div class="meta">
+			<?php foreach($categories as $cat)
+				{ 
+				  	echo $cat->cat_name; 
+				}
+			?>&nbsp;|&nbsp;<span class="common-cat">NEWS</span>
+		</div>
+		</div>
+		<a href="<?php echo get_permalink($recent[" ID"])?>" title="Look <?php echo esc_attr($recent[" post_title"])?>" class="check_now">Check now</a>
+	</div>
+</li>
+
+<?php
+}
 ?>
 </ul>
