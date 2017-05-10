1154f03eee7c5e45787cf77b44a06a46b5ebdf7c
thangtqvn <quocthang.2001@gmail.com>
2017-05-10 23:52:05 +0700

    Upload files

--- wp-content/themes/realty-child/template-parts/content-news.php
+++ wp-content/themes/realty-child/template-parts/content-news.php
@@ -1,4 +1,5 @@
 <?php 
+$news_post = get_post();
 $building_id = get_post_meta(get_the_ID(), 'jpdb_building_id', true);
 if ($building_id)
 {
@@ -24,9 +25,9 @@ if ($building_id)
 
 	}
 }
-$news_url = isset($new_property) ? get_permalink($new_property->ID) : get_permalink();
+$news_url = isset($new_property) ? get_permalink($new_property->ID) : get_permalink($news_post);
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php $news_post->ID; ?>" <?php post_class('', $news_post->ID); ?>>
 
 <div class="entry-header">
 
@@ -35,22 +36,22 @@ $news_url = isset($new_property) ? get_permalink($new_property->ID) : get_permal
 		
 
 		<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h1 class="entry-title"><a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php echo $news_post->post_title; ?></a></h1>
 		<?php else : ?>
-			<h2 class="entry-title"><a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<h2 class="entry-title"><a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php echo $news_post->post_title; ?></a></h2>
 		<?php endif; ?>
 
 		<div class="header-meta">
-			<div class="post-date"><?php echo get_post_time('F d, Y'); ?>
+			<div class="post-date"><?php echo get_post_time('F d, Y', $news_post->ID); ?>
 </div>
 		</div>
 
 	</div>
 
 </div><!-- .entry-header -->
-<?php if ( has_post_thumbnail() ) { ?>
+<?php if ( has_post_thumbnail($news_post->ID) ) { ?>
 			<div class="header-media">
-				<a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php the_post_thumbnail( 'thumbnail-16-9' ); ?></a>
+				<a href="<?php echo esc_url( $news_url ); ?>" rel="bookmark"><?php echo get_the_post_thumbnail($news_post->ID, 'thumbnail-16-9' ); ?></a>
 			</div>
 		<?php } ?>
 <?php echo tt_post_content_navigation(); ?>
