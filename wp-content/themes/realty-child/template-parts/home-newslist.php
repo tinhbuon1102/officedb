8b503189f068d83fafb9cc8123b8e99325c98229
thangtqvn <quocthang.2001@gmail.com>
2017-05-10 21:50:56 +0700

    Upload files

--- wp-content/themes/realty-child/template-parts/home-newslist.php
+++ wp-content/themes/realty-child/template-parts/home-newslist.php
@@ -6,6 +6,33 @@ $recent_posts = wp_get_recent_posts(array(
 
 foreach ( $recent_posts as $recent )
 {
+	$building_id = get_post_meta($recent['ID'], 'jpdb_building_id', true);
+	if ($building_id)
+	{
+		// Get property by news
+		$new_args = array(
+			'post_type' => 'property',
+			'posts_per_page' => 1,
+			'meta_query' => array(
+				array(
+					'key' => FLOOR_BUILDING_TYPE,
+					'value' => $building_id,
+				)
+			)
+		);
+		$the_news_query = new WP_Query( $new_args );
+		if ( $the_news_query->have_posts() )
+		{
+			while ( $the_news_query->have_posts() ) {
+				$the_news_query->the_post();
+				global $post;
+				$new_property = $post;
+			}
+		
+		}
+	}
+	
+	
 	$categories = get_the_category($recent['ID']); // post ID
 	foreach ( $categories as $cat )
 	{
@@ -27,7 +54,7 @@ foreach ( $recent_posts as $recent )
 					?>
 				</div>
 			</div>
-			<a href="<?php echo get_permalink($recent['ID'])?>" title="Look <?php echo esc_attr($recent[" post_title"])?>" class="check_now">Check now</a>
+			<a href="<?php echo isset($new_property) ? get_permalink($new_property->ID) : get_permalink($recent['ID'])?>" title="Look <?php echo esc_attr($recent[" post_title"])?>" class="check_now">Check now</a>
 		</div>
 	</li>
 
