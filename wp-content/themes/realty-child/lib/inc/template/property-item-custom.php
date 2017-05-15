85b3eedbad3f35299c012f3351929ecbe1ecc35d
thangtqvn <quocthang.2001@gmail.com>
2017-05-15 22:48:49 +0700

    Upload files

--- wp-content/themes/realty-child/lib/inc/template/property-item-custom.php
+++ wp-content/themes/realty-child/lib/inc/template/property-item-custom.php
@@ -90,6 +90,7 @@ $last_updated_on = get_post_modified_time( get_option( 'date_format' ) );
 					<?php else:  /* Japanese */ ?>
 					<?php echo $estate_property_prefecture ? ($estate_property_prefecture .''. $estate_property_district . '' . $estate_property_town) : ""; ?>
 					<?php endif; ?>
+					<?php if (is_user_logged_in()) echo trans_text(sprintf('(Views : %s)', (int)get_post_meta(get_the_ID(), 'estate_property_views_count', true)));?>
 					</h4>
 					<?php the_excerpt(); ?>
 					</div>
