edc99c6e81fe970b8bf34d716f4efc94559aa72a
thangtqvn <quocthang.2001@gmail.com>
2017-07-13 16:30:46 +0700

    Upload files

--- wp-content/plugins/realty-core/functions-scripts.php
+++ wp-content/plugins/realty-core/functions-scripts.php
@@ -1,6 +1,29 @@
 <?php
 if ( ! function_exists( 'tt_script_slick_slider' ) ) {
 	function tt_script_slick_slider( $slider_params ) {
+		switch($slider_params['id'])
+		{
+			case 'similar_properties_carousel':
+				$small = 1;
+				$medium = 2;
+				$large = 3;
+				$huge = 4;
+				break;
+		
+			case 'property_image_slider':
+				$small = 1;
+				$medium = 1;
+				$large = 1;
+				$huge = 1;
+				break;
+				
+			default:
+				$small = $slider_params['images_to_show_sm'];
+				$medium = $slider_params['images_to_show_md'];
+				$large = $slider_params['images_to_show_lg'];
+				$huge = $slider_params['images_to_show_lg'];
+				break;
+		}
 		?>
 		<script>
 			(function($) {
@@ -75,25 +98,25 @@ if ( ! function_exists( 'tt_script_slick_slider' ) ) {
 			        	{
 				            breakpoint: 481,
 				            settings: {
-				              slidesToShow: 1,
+				              slidesToShow: <?php echo $small; ?>,
 				            }
 				          },
 			          {
 			            breakpoint: 591,
 			            settings: {
-			              slidesToShow: 2,
+			              slidesToShow: <?php echo $medium; ?>,
 			            }
 			          },
 			          {
 			            breakpoint: 992,
 			            settings: {
-			              slidesToShow: 3,
+			              slidesToShow: <?php echo $large; ?>,
 			            }
 			          },
 			          {
 			            breakpoint: 1200,
 			            settings: {
-			              slidesToShow: <?php echo $slider_params['images_to_show_lg']; ?>,
+			              slidesToShow: <?php echo $huge; ?>,
 			            }
 			          },
 			        ],
