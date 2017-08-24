df95be374587d2f7827d183bacad9321d9cac1a8
tinhbuon1102 <quocthang.2001@gmail.com>
2017-08-24 20:54:50 +0900

    Server Upload files

--- wp-content/themes/realty-child/lib/inc/template/login-bar-header.php
+++ wp-content/themes/realty-child/lib/inc/template/login-bar-header.php
@@ -48,7 +48,7 @@
 		$number_of_favorites = $number_of_favorites / 2;
 	?>
 	
-	<div class="demo jktCD"> <span class="jktCD-click"><?php echo isEnglish() ? 'For' : ''; ?> <?php echo get_user_meta($user_id, 'user_name', true)?><?php echo isEnglish() ? '' : '様'; ?></span>
+	<div class="demo jktCD"> <span class="jktCD-click"><?php echo isEnglish() ? 'For:' : ''; ?> <?php echo get_user_meta($user_id, 'user_name', true)?><?php echo isEnglish() ? '' : '様'; ?></span>
 <div class="jktCD-main jktCD-style-one">
 <ul>
 
