7280934da148e7848e18c639401c6b434d8904ed
thangtqvn <quocthang.2001@gmail.com>
2017-09-02 00:07:23 +0700

    Upload files

--- wp-content/themes/realty-child/init_style.php
+++ wp-content/themes/realty-child/init_style.php
@@ -810,4 +810,23 @@ body.page-template-page-lgray {
     font-family: "heisei-mincho-stdn","Roboto Slab",Garamond,"Times New Roman",游明朝,"Yu Mincho",游明朝体,YuMincho,"ヒラギノ明朝 Pro W3","Hiragino Mincho Pro",HiraMinProN-W3,HGS明朝E,"ＭＳ Ｐ明朝","MS PMincho",serif;
     font-family: "Roboto Slab",Garamond,"Times New Roman",游明朝,"Yu Mincho",游明朝体,YuMincho,"ヒラギノ明朝 Pro W3","Hiragino Mincho Pro",HiraMinProN-W3,HGS明朝E,"ＭＳ Ｐ明朝","MS PMincho",serif;
 }
+@media (min-width: 992px) {
+.col-md-4 {width: 33.33333%;}
+}
+@media (min-width: 992px) {
+.col-md-8 {
+    width: 66.66667%;
+}
+}
+@media (min-width: 992px) {
+.col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12 {
+    float: left;
+}
+}
+.col-xs-1, .col-sm-1, .col-md-1, .col-lg-1, .col-xs-2, .col-sm-2, .col-md-2, .col-lg-2, .col-xs-3, .col-sm-3, .col-md-3, .col-lg-3, .col-xs-4, .col-sm-4, .col-md-4, .col-lg-4, .col-xs-5, .col-sm-5, .col-md-5, .col-lg-5, .col-xs-6, .col-sm-6, .col-md-6, .col-lg-6, .col-xs-7, .col-sm-7, .col-md-7, .col-lg-7, .col-xs-8, .col-sm-8, .col-md-8, .col-lg-8, .col-xs-9, .col-sm-9, .col-md-9, .col-lg-9, .col-xs-10, .col-sm-10, .col-md-10, .col-lg-10, .col-xs-11, .col-sm-11, .col-md-11, .col-lg-11, .col-xs-12, .col-sm-12, .col-md-12, .col-lg-12 {
+    position: relative;
+    min-height: 1px;
+    padding-left: 15px;
+    padding-right: 15px;
+}
 </style>
