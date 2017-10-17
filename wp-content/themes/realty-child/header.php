7ca8fa855e6fc34ba3ae60ef5078d0e98a89a294
thangtqvn <quocthang.2001@gmail.com>
2017-10-17 14:54:07 +0700

    Upload files

--- wp-content/themes/realty-child/header.php
+++ wp-content/themes/realty-child/header.php
@@ -62,6 +62,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <body <?php body_class(); ?>>
 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRFCLLL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <?php global $realty_theme_option; ?>
 <div class="viewwrap">
 <header id="header"<?php if ( $realty_theme_option['header-layout'] == 'default' ) { echo esc_attr( ' class=nav-below' ); } ?>>
